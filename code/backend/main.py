f6f50a925b28cc27622b491e71a5ddbefdd3faad
PFDX <1253467775@qq.com>
2025-01-15 13:57:28 +0800

    后端基本接口函数实现

--- code/backend/main.py
+++ code/backend/main.py
@@ -21,66 +21,114 @@ with app.app_context():
         print(rs.fetchone())
 
 class USER(db.Model):#固定写法
-    __tabelname__ = "USER"
+    __table_name__ = "USER"
     #id = db.Column(db.Integer, primary_key = True, autoincrement = True)#用不上或许？
-    username = db.Column(db.String(100), primary_key = True, nullable = False)
+    user_name = db.Column(db.String(100), primary_key = True, nullable = False)
     password = db.Column(db.String(100), nullable = False)
 
 #user = USER(username = "somebody", password = "123")
-#自动映射 sql: insert user(username, password) values ("somebody", "123")
 
 class DETAIL(db.Model):
-    __tabelname__ = "DETAIL"
-    username = db.Column(db.String(100), nullable = False)
+    __table_name__ = "DETAIL"
+    user_name = db.Column(db.String(100), nullable = False)
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     
 
 with app.app_context():#获取上下文
     db.create_all()
 
+
+# @app.route('/api/user/add')
+def user_add(user_name, password):
+# 添加用户名
+    new_one = USER(user_name=user_name, password=password)
+    db.session.add(new_one)
+    db.session.commit()
+    return "success"
+
+# @app.route('/api/user/query')
+def user_query(user_name, password):
+#查询是否存在该用户
+
+    users = USER.query.filter_by(user_name=user_name)# 得到一个Query对象，用于返回某人全部数据，可以使用for in
+    if users.count() == 0:
+        return -2#用户不存在
+    else:
+        if users.first().password == password:
+            return 0#成功
+        else:
+            return -1#密码错误
+
+
 @app.route('/')
 
 def print_hi():
     return "<p>Hi, my name is HarmonyOS</p>"
 
-@app.route('/api/list')#检验是否用户名及密码
-def list():
-    return 0
+# @app.route('/api/list')#检验是否用户名及密码
+# def list():
+#     return 0
 
 
 @app.route('/api/detail/<string:id>')#或许是某个用户？
-def detail(id):
+def detail(user_id):
     return id
     
-@app.route('/api/user/add')
-def user_add():
-    new_one = USER(username = "someone", password = "111222333")
-    db.session.add(new_one)
-    db.session.commit()
-    return "new one added"
-
-@app.route('/api/user/query')
-def user_query():
-    #user = USER.query.get("someone")#get主键查找，用于查找账号密码,得到一条数据
-    users = USER.query.filter_by(username = "someone")#得到一个Query对象，用于返回某人全部数据，可以使用for in
-    return "query"
-    
-@app.route('/api/user/update')
+@app.route('/api/update_user')
+#实现修改密码
 def user_update():
-    user = USER.query.filter_by(username = "someone")
-    user.password = "new"
+    json_data = request.get_json()
+    user_name = json_data['user_name']
+    password = json_data['password']
+    user = USER.query.filter_by(user_name=user_name).first()
+    user.password = password
     db.session.commit()
-    return "update"
+    return "success"
 
-@app.route('/api/detail/delete/<integer:id>')
-def detail_delete(id):
-    
+@app.route('/api/add')
+def detail_add():
+    json_data = request.get_json()
+    #todo
+
+@app.route('/api/delete')
+def detail_delete():
+    json_data = request.get_json()
     with db.engine.connect() as conn:
         rs = conn.execute(text(f"delete from detail where id = {id}"))
-    #detail = DETAIL.query.filter_by(id = 2).first()
-    #db.session.delete(detail)
-    #db.session.commit()
+    detail = DETAIL.query.filter_by(id = 2).first()
+    db.session.delete(detail)
+    db.session.commit()
     return "delete"
+@app.route('/api/update')
+def detail_update():
+    json_data = request.get_json()
+    #todo
+
+# @app.route('/api/detail/delete/<int:id>')
+# def detail_delete(id):
+#     with db.engine.connect() as conn:
+#         rs = conn.execute(text(f"delete from detail where id = {id}"))
+#     # detail = DETAIL.query.filter_by(id = 2).first()
+#     # db.session.delete(detail)
+#     # db.session.commit()
+#     return "delete"
+
+@app.route('/api/sign')
+def sign():
+    json_data = request.get_json()
+    is_sign_up = json_data['is_sign_up']
+    user_name = json_data['user_name']
+    password = json_data['password']
+    if is_sign_up:
+        user_add(user_name,password)
+    else:
+        if user_query(user_name,password)==-2:
+            return "该用户不存在"
+        elif user_query(user_name,password)==-1:
+            return "密码错误"
+        else:
+            #todo 传递数据
+            return "登陆成功！"
 
 if __name__ == '__main__':
     app.run(debug=True)
