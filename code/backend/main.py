6d8cb6dbcbb06be98c4d718bc34190cb0077ab6a
PFDX <1253467775@qq.com>
2025-01-17 12:49:52 +0800

    后端代码风格修正

--- code/backend/main.py
+++ code/backend/main.py
@@ -5,14 +5,14 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-HOSTNAME = "127.0.0.1"
-PORT = 3306
-USERNAME = "root"
+HOSTNAME = "127.0.0.1" # 数据库host
+PORT = 3306 # 数据库端口号
+USERNAME = "root" # 用户名
 PASSWORD = "mysql123" # mysql密码   
 DATABASE = "harmony"  # 数据库名称
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}?charset=utf8mb4"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}?charset=utf8mb4" #配置
 
 db = SQLAlchemy(app)#读取app.config
 
@@ -29,8 +29,6 @@ class USER(db.Model):#固定写法
     user_name = db.Column(db.String(100), primary_key = True, nullable = False)
     password = db.Column(db.String(100), nullable = False)
 
-#user = USER(username = "somebody", password = "123")
-
 class DETAIL(db.Model):
     __table_name__ = "DETAIL"
 
@@ -40,23 +38,19 @@ class DETAIL(db.Model):
     money = db.Column(db.Float, nullable = False)
     content = db.Column(db.String(200))
     date = db.Column(db.String(100), nullable = False)
-    
 
 with app.app_context():#获取上下文
     db.create_all()
 
-@app.route("/test")
+@app.route("/test")#单元测试用接口
 def test():
-    new_one = DETAIL(user_name = "hxy", bill_type = 0, money = 100000000, content = "", date = "2025-01-15")
-    db.session.add(new_one)
-    new_one = DETAIL(user_name = "hxy", bill_type = 1, money = 10000, content = "hehe", date = "2025-01-14")
-    db.session.add(new_one)
-    new_one = DETAIL(user_name = "hxy", bill_type = 0, money = 10, content = "pc", date = "2025-01-16")
-    db.session.add(new_one)
-    new_one = DETAIL(user_name = "zbc", bill_type = 0, money = 100, content = "hxy", date = "2025-01-15")
-    db.session.add(new_one)
-    db.session.commit()
-
+    # new_one = DETAIL(user_name = "hxy", bill_type = 0, money = 100000000, content = "", date = "2025-01-15")
+    # db.session.add(new_one)
+    # db.session.commit()
+    # assert (get_data("hxy").get_json()["datas"][0]["money"] == 100000000)
+    # detail = DETAIL.query.filter_by(user_name="hxy").first()
+    # db.session.delete(detail)
+    # db.session.commit()
     return "200"
 
 # @app.route('/api/user/add')
@@ -81,20 +75,14 @@ def user_query(user_name, password):
         else:
             return -1#密码错误
 
-
 @app.route('/')
-
 def print_hi():
     return "<p>Hi, my name is HarmonyOS</p>"
 
-# @app.route('/api/list')#检验是否用户名及密码
-# def list():
-#     return 0
 
-
-@app.route("/api/detail/<string:id>")#或许是某个用户？
-def detail(user_id):
-    return id
+# @app.route("/api/detail/<string:id>")#或许是某个用户？
+# def detail(user_id):
+#     return id
     
 @app.route("/api/update_user")
 def user_update():
@@ -113,9 +101,9 @@ def user_update():
 @app.route("/api/add", methods=["POST"])
 def detail_add():
     #添加某条数据
-    print("dadadadadadadad")
+    # print("add!")
     json_data = request.get_json()
-
+    # 读取数据
     user_name = json_data["user_name"]
     bill_type = json_data["type"]
     money = json_data["money"]
@@ -132,9 +120,9 @@ def detail_add():
 def detail_delete():
     #删除某条数据
     json_data = request.get_json()
-    id = json_data["id"]
+    user_id = json_data["id"]
     
-    detail = DETAIL.query.filter_by(id = id).first()
+    detail = DETAIL.query.filter_by(id = user_id).first()
     db.session.delete(detail)
     db.session.commit()
     return "200"
@@ -143,14 +131,14 @@ def detail_delete():
 def detail_update():
     #修改某条数据
     json_data = request.get_json()
-
-    id = json_data["id"]
+    #读取数据
+    user_id = json_data["id"]
     bill_type = json_data["type"]
     money = json_data["money"]
     content = json_data["content"]
     date = json_data["date"]
-
-    detail = DETAIL.query.filter_by(id = id).first()
+    #写入数据
+    detail = DETAIL.query.filter_by(id = user_id).first()
     detail.bill_type = bill_type
     detail.money = money
     detail.content = content
@@ -159,24 +147,22 @@ def detail_update():
 
     return "200"
 
-from flask import jsonify
-
 @app.route("/api/sign", methods=["POST"])
 def sign():
-    print("sign")
+    # print("sign")
     json_data = request.get_json()
-
+    # 读取数据
     is_sign_up = json_data["is_sign_up"]
     user_name = json_data["user_name"]
     password = json_data["password"]
     print(f"getdata:\nis_sign_up:{is_sign_up}\nuser_name:{user_name}\npassword:{password}")
 
     if is_sign_up:
-        print("开始注册")
+        # print("开始注册")
         # 下面的判断用户已存在存在问题,如需要查看可以注释掉下面两行
         if user_query(user_name, password) != -2:
             return jsonify({"status": -3, "message": "账号已存在"})  # 需要返回 JSON 格式
-        print("没有这个用户")
+        # print("没有这个用户")
         # 注册用户
         user_add(user_name, password)
         return jsonify({"status": 0, "message": "注册成功"})
